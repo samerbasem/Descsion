50a20996ea7fb696830c564f8963070d8c345df8
eng nada <nada31181@gmail.com>
2023-09-17 15:03:59 +0300

    ww

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Login from '../views/Login.vue'
+import Login from '../views/login.vue'
 import FollowUpDeesion from '../views/FollowUpDeesion.vue'
 Vue.use(VueRouter)
 
